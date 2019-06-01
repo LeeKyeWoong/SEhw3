7d3eae6595adff5353a145dc1e303b19843feaf7
LEEKYEWOONG <leekw0108@gmail.com>
2019-06-02 02:33:31 +0900

    Success 판매티켓조회

--- RegisterTicketControl.h
+++ RegisterTicketControl.h
@@ -4,6 +4,8 @@
 #include <string>
 #include "main.h"
 
+using namespace std;
+
 class MemberCollection;
 class TicketCollection;
 class Timer;
@@ -16,11 +18,11 @@ class RegisterTicketControl
 	// Author: 이계웅
 
 private:
-
-	TicketCollection * ticketCollection; 
-	MemberCollection * memberCollection; 
+	bool checkRegister;
 
 public:
+	RegisterTicketControl();
 	void registerNewTicket(int price, string gameDatenTime, string homeTeam, string awayTeam, string seatNum, bool isLimitedTimeAuction, TicketCollection *pticketCollection, MemberCollection * pmemberCollection ,Timer * ptimer);  //  TicketCollection에 티켓을 추가하는함수
+	bool getCheckRegister(); // 티켓등록을 시도하는 티켓의 중복 여부를 반환하는 함수
 };
 
